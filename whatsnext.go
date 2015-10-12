76a60dbe5095d063becc74b6a92ca3a7f7c076f2
Aaron Bedra <aaron@aaronbedra.com>
2015-10-12 07:19:47 -0500

    Allow multiple users in config.

--- whatsnext.go
+++ whatsnext.go
@@ -2,16 +2,16 @@ package main
 
 import (
         "fmt"
-	"io/ioutil"
-	"flag"
+        "io/ioutil"
+        "flag"
         "github.com/google/go-github/github"
         "golang.org/x/oauth2"
-	"gopkg.in/yaml.v2"
+        "gopkg.in/yaml.v2"
 )
 
 type Config struct {
-	User  string
-	Token string
+        Users  []string
+        Token  string
 }
 
 func printIssue(issue github.Issue) {
@@ -23,8 +23,8 @@ func printIssue(issue github.Issue) {
 }
 
 func main() {
-	configPtr := flag.String("config", "config.yml", "Path to configuration yaml file")
-	flag.Parse()
+        configPtr := flag.String("config", "config.yml", "Path to configuration yaml file")
+        flag.Parse()
 
         var config Config
         raw, err := ioutil.ReadFile(*configPtr)
@@ -40,23 +40,23 @@ func main() {
         ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: config.Token},)
         tc := oauth2.NewClient(oauth2.NoContext, ts)
         client := github.NewClient(tc)
-        repos, _, err := client.Repositories.List(config.User, nil)
+        for _, user := range config.Users {
+                repos, _, err := client.Repositories.List(user, nil)
 
-        if err != nil {
-                fmt.Println("Error:", err)
-        } else {
-                fmt.Printf("%s repositories\n", config.User)
-                for _, repo := range repos {
-                        fmt.Printf("  %s\n", *repo.Name)
-                        issues, _, err := client.Issues.ListByRepo(config.User, *repo.Name, nil)
-                        if err != nil {
-                                fmt.Println("Error:", err)
-                        } else {
-                                if len(issues) == 0 {
-                                        fmt.Printf("    No Issues\n")
+                if err != nil {
+                        fmt.Println("Error:", err)
+                } else {
+                        fmt.Printf("Open issues for %s\n", user)
+                        for _, repo := range repos {
+                                issues, _, err := client.Issues.ListByRepo(user, *repo.Name, nil)
+                                if err != nil {
+                                        fmt.Println("Error:", err)
                                 } else {
-                                        for _, issue := range issues {
-                                                printIssue(issue)
+                                        if len(issues) != 0 {
+                                                fmt.Printf("  %s\n", *repo.Name)
+                                                for _, issue := range issues {
+                                                        printIssue(issue)
+                                                }
                                         }
                                 }
                         }
