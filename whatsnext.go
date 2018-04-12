502d743241916fdfde5f8eb9ff833daa74e49d92
Matt Konda <mkonda@jemurai.com>
2018-04-12 09:40:41 -0500

    Add context references to get this working with current API.

--- whatsnext.go
+++ whatsnext.go
@@ -1,84 +1,86 @@
 package main
 
 import (
-        "fmt"
-        "io/ioutil"
-        "flag"
-        "os"
-        "github.com/google/go-github/github"
-        "golang.org/x/oauth2"
-        "gopkg.in/yaml.v2"
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
-        Users  []string
-        Token  string
+	Users []string
+	Token string
 }
 
 func printIssue(issue *github.Issue) {
-        if *issue.Number < 10 {
-                fmt.Printf("    [%d]  - %s\n", *issue.Number, *issue.Title)
-        } else {
-                fmt.Printf("    [%d] - %s\n", *issue.Number, *issue.Title)
-        }
+	if *issue.Number < 10 {
+		fmt.Printf("    [%d]  - %s\n", *issue.Number, *issue.Title)
+	} else {
+		fmt.Printf("    [%d] - %s\n", *issue.Number, *issue.Title)
+	}
 }
 
 func ReadConfig(file string) Config {
-        var config Config
+	var config Config
 
-        raw, err := ioutil.ReadFile(file)
-        if err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-        }
+	raw, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-        err = yaml.Unmarshal(raw, &config)
-        if err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-        }
+	err = yaml.Unmarshal(raw, &config)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-        return config
+	return config
 }
 
 func BuildClient(config Config) *github.Client {
-        ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: config.Token},)
-        tc := oauth2.NewClient(oauth2.NoContext, ts)
-        client := github.NewClient(tc)
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: config.Token})
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	client := github.NewClient(tc)
 
-        return client
+	return client
 }
 
 func ProcessRepositories(config Config, client *github.Client) {
-        for _, user := range config.Users {
-                repos, _, err := client.Repositories.List(user, nil)
+	ctx := context.Background()
+	for _, user := range config.Users {
+		repos, _, err := client.Repositories.List(ctx, user, nil)
 
-                if err != nil {
-                        fmt.Println("Error:", err)
-                } else {
-                        fmt.Printf("Open issues for %s\n", user)
-                        for _, repo := range repos {
-                                issues, _, err := client.Issues.ListByRepo(user, *repo.Name, nil)
-                                if err != nil {
-                                        fmt.Println("Error:", err)
-                                } else {
-                                        if len(issues) != 0 {
-                                                fmt.Printf("  %s\n", *repo.Name)
-                                                for _, issue := range issues {
-                                                        printIssue(issue)
-                                                }
-                                        }
-                                }
-                        }
-                }
-        }
+		if err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Printf("Open issues for %s\n", user)
+			for _, repo := range repos {
+				issues, _, err := client.Issues.ListByRepo(ctx, user, *repo.Name, nil)
+				if err != nil {
+					fmt.Println("Error:", err)
+				} else {
+					if len(issues) != 0 {
+						fmt.Printf("  %s\n", *repo.Name)
+						for _, issue := range issues {
+							printIssue(issue)
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
 func main() {
-        configPtr := flag.String("config", "config.yml", "Path to configuration yaml file")
-        flag.Parse()
-
-        config := ReadConfig(*configPtr)
-        client := BuildClient(config)
-        ProcessRepositories(config, client)
+	configPtr := flag.String("config", "config.yml", "Path to configuration yaml file")
+	flag.Parse()
+	config := ReadConfig(*configPtr)
+	client := BuildClient(config)
+	ProcessRepositories(config, client)
 }
